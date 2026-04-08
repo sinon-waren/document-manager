990bc61f121fbb3e14231bec026d00c199982da1
sinon-waren <sinonwaren2000@gmail.com>
2026-04-08 11:06:43 +0200

    fix conflict import dependency and config version spring

---
+++ src/test/java/com/ministere/document_management/controller/DemandeControllerTest.java
@@ -0,0 +1,24 @@
+package com.ministere.document_management.controller;
+
+import com.ministere.document_management.dto.DemandeRequestDto;
+import com.ministere.document_management.dto.DemandeResponseDto;
+import com.ministere.document_management.entity.Demande;
+import com.ministere.document_management.service.DemandeService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDate;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+public class DemandeControllerTest {
+    
+}
--- src/test/java/com/ministere/document_management/repository/DemandeRepositoryTest.java
+++ src/test/java/com/ministere/document_management/repository/DemandeRepositoryTest.java
@@ -6,7 +6,7 @@ import com.ministere.document_management.entity.Demande;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
